0b6aac30f1f87457ac55903d211701ab22f14ebf
Muktar Sadiq <muktarsadiq.m@yahoo.com>
2022-08-23 17:47:35 +0100

    first commit

---
+++ components/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import AboutImg from '../public/assets/about.jpg';
+
+const About = () => {
+  return (
+    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
+      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+        <div className='col-span-2'>
+          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+            About
+          </p>
+          <h2 className='py-4'>Who I Am</h2>
+          <p className='py-2 text-gray-600'>
+            / / I am not your average developer
+          </p>
+          <p className='py-2 text-gray-600'>
+            Hi i'm Muktar sadiq, i am a web  developer, specializing in Front-end development.
+            i  have a Bachelor's Degree in Physics,i love coding, reading and learning new things.
+           
+          </p>
+          <p className='py-2 text-gray-600'>
+            Fascinated with how intricate programming can be, my mission is  to design solutions that improve the quality of lives of millions of peopleand to grow and evovle both in my stack and as a person in general.
+          </p>
+          <Link href='/#projects'>
+            <p className='py-2 text-gray-600 underline cursor-pointer'>
+              Check out some of my latest projects.
+            </p>
+          </Link>
+        </div>
+        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+          <Image src={AboutImg} className='rounded-xl' alt='/' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
